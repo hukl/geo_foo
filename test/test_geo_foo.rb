a57fe2584adbdc1a61d2a4b728a81f4e5154b831
hukl <contact@smyck.org>
2010-03-09 21:29:38 +0100

    Initial commit to geo_foo.

---
+++ test/test_geo_foo.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestGeoFoo < MiniTest::Unit::TestCase
+  def test_something_for_real
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
