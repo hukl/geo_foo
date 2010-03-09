c1a353f7da525c09ee0494936d370e09e36ad22d
hukl <contact@smyck.org>
2010-03-09 23:03:22 +0100

    added basic extension mechanism

--- lib/geo_foo/active_record.rb
+++ lib/geo_foo/active_record.rb
@@ -1,14 +1,26 @@
 module GeoFoo
   module ActiveRecord
     def self.included(base)
-      raise
       base.extend ClassMethods
     end
   end
   
   module ClassMethods
-    def hello
+    def add_geo_foo
       puts "ar"
+      self.named_scope :within_radius, lambda { |lat, lon, radius|
+        {
+          :conditions => [
+            "ST_DWithin(#{GeoFoo::Core.as_point(lat,lon)}, point, #{bbox_size(lat, radius)})"\
+            " AND ST_Distance_Sphere(point, #{GeoFoo::Core.as_point(lat,lon)}) < #{radius}"
+          ]
+        }
+      }
+    end
+    
+    # XXX handle case where lat is (close to) +-90deg (poles)
+    def bbox_size latitude, radius
+      (radius.to_f / (GeoFoo::Core::EarthRadius * Math.cos(latitude.to_rad))).to_deg
     end
   end
 end
