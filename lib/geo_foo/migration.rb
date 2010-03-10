bf10e69b8b2f78538d49ed8b80a4ad5e75d30187
hukl <contact@smyck.org>
2010-03-10 11:49:09 +0100

    updated migration

--- lib/geo_foo/migration.rb
+++ lib/geo_foo/migration.rb
@@ -2,14 +2,14 @@ module GeoFoo
   module ActiveRecord
     module Migration
       # migration helper: create the database table
-      def self.create_posgis_point_table table_name
+      def create_posgis_point_table table_name
         self.connection.execute "CREATE TABLE #{table_name} (id serial PRIMARY KEY)"
         self.connection.execute "SELECT AddGeometryColumn('#{table_name}', 'point', #{SRID}, 'POINT', 2)"
         self.connection.execute "CREATE INDEX #{table_name}_point_index ON #{table_name} USING GIST (point)"
       end
 
       # migration helper: drop the database table
-      def self.drop_posgis_point_table table_name
+      def drop_posgis_point_table table_name
         self.connection.execute "DROP TABLE #{table_name}"
       end
     end
