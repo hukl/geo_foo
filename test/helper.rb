7fc955b1a2ca5f6700ccbd69c222785c8ba3e24e
David Siegel <david.siegel@artcom.de>
2010-03-10 21:36:06 +0100

    added test database infrastructure

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,11 @@
 require 'rubygems'
 require 'minitest/unit'
+require 'active_record'
+
+config_file = File.join(File.dirname(__FILE__), '..', 'test_database.yml')
+database_config = YAML.load_file config_file
+database_config[:adapter] = 'postgresql'
+ActiveRecord::Base.establish_connection database_config
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
--- test/test_geo_foo.rb
+++ test/test_geo_foo.rb
@@ -1,7 +1,25 @@
 require 'helper'
 
 class TestGeoFoo < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def execute sql
+    ActiveRecord::Base.connection.execute sql
+  end
+
+  def query sql
+    ActiveRecord::Base.connection.query sql
+  end
+
+  def query_scalar sql
+    query(sql)[0][0]
+  end
+
+  def test_database_connection
+    # perform a query first to 'wake-up' the connection
+    assert_equal(query_scalar("SELECT 23").to_i, 23)
+    assert ActiveRecord::Base.connected?, "database connection esteblished"
+  end
+
+  def test_postgis_database
+    assert_equal(query_scalar('select count(*) from geometry_columns').to_i, 0)
   end
 end
