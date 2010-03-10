58a1153f43fd0bea3f9355dd5f0b7a64d299a814
David Siegel <david.siegel@artcom.de>
2010-03-10 22:51:07 +0100

    added point test

--- test/test_geo_foo.rb
+++ test/test_geo_foo.rb
@@ -1,6 +1,26 @@
+require 'geo_foo/core'
 require 'helper'
 
 class TestGeoFoo < MiniTest::Unit::TestCase
+  def test_database_connection
+    # perform a query first to 'wake-up' the connection
+    assert_equal(query_scalar("SELECT 5").to_i, 5)
+    assert ActiveRecord::Base.connected?, "database connection esteblished"
+  end
+
+  def test_postgis_database
+    assert(query('SELECT count(*) FROM geometry_columns'))
+  end
+
+  def test_as_point
+    latitude = 5
+    longitude = 42
+    point = GeoFoo::Core.as_point latitude, longitude
+    assert(query("SELECT #{point}"))
+    assert_equal(query_scalar("SELECT ST_X(#{point})").to_i, 42)
+    assert_equal(query_scalar("SELECT ST_Y(#{point})").to_i, 5)
+  end
+
   def execute sql
     ActiveRecord::Base.connection.execute sql
   end
@@ -11,15 +31,5 @@ class TestGeoFoo < MiniTest::Unit::TestCase
 
   def query_scalar sql
     query(sql)[0][0]
-  end
-
-  def test_database_connection
-    # perform a query first to 'wake-up' the connection
-    assert_equal(query_scalar("SELECT 23").to_i, 23)
-    assert ActiveRecord::Base.connected?, "database connection esteblished"
-  end
-
-  def test_postgis_database
-    assert_equal(query_scalar('select count(*) from geometry_columns').to_i, 0)
   end
 end
