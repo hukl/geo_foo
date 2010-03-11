20dde30be0ae2c3e3e757f468473783a1dd048b1
hukl <contact@smyck.org>
2010-03-11 11:18:02 +0100

    using ActiveSupport::TestCase as the whole thing is somehow bound to rails anyway

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'test/unit'
 require 'active_record'
+require 'active_support'
+require 'active_support/test_case'
 
 config_file = File.join(File.dirname(__FILE__), '..', 'test_database.yml')
 database_config = YAML.load_file config_file
@@ -11,5 +13,19 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'geo_foo'
 
-class Test::Unit::TestCase
+class ActiveSupport::TestCase
+  
+  def execute sql
+    ActiveRecord::Base.connection.execute sql
+  end
+
+  def query sql
+    ActiveRecord::Base.connection.query sql
+  end
+
+  def query_scalar sql
+    query(sql).first.first
+  end
+  
+  
 end
--- test/test_geo_foo.rb
+++ test/test_geo_foo.rb
@@ -1,18 +1,19 @@
 require 'geo_foo/core'
 require 'helper'
 
-class TestGeoFoo < Test::Unit::TestCase
-  def test_database_connection
+class TestGeoFoo < ActiveSupport::TestCase
+
+  test "database connection" do
     # perform a query first to 'wake-up' the connection
     assert_equal(query_scalar("SELECT 5").to_i, 5)
     assert ActiveRecord::Base.connected?, "database connection esteblished"
   end
 
-  def test_postgis_database
+  test "postgis database is present" do
     assert(query('SELECT count(*) FROM geometry_columns'))
   end
 
-  def test_as_point
+  test "as_point" do
     latitude = 5
     longitude = 42
     point = GeoFoo::Core.as_point latitude, longitude
@@ -20,16 +21,5 @@ class TestGeoFoo < Test::Unit::TestCase
     assert_equal(query_scalar("SELECT ST_X(#{point})").to_i, 42)
     assert_equal(query_scalar("SELECT ST_Y(#{point})").to_i, 5)
   end
-
-  def execute sql
-    ActiveRecord::Base.connection.execute sql
-  end
-
-  def query sql
-    ActiveRecord::Base.connection.query sql
-  end
-
-  def query_scalar sql
-    query(sql).first.first
-  end
+  
 end
