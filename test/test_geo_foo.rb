0fc7541c9361ec1a3637a8d51336c4a45944c801
hukl <contact@smyck.org>
2010-03-15 20:54:59 +0100

    added setup and teardown methods to create and drop test table + columns

--- test/test_geo_foo.rb
+++ test/test_geo_foo.rb
@@ -1,8 +1,23 @@
 require 'geo_foo/core'
 require 'helper'
+require 'models/location'
 
 class TestGeoFoo < ActiveSupport::TestCase
-
+  
+  def setup
+      ActiveRecord::Base.connection.execute(
+        "CREATE TABLE locations (
+           point geometry
+         );"
+      )
+  end
+  
+  def teardown
+    ActiveRecord::Base.connection.execute(
+      "DROP TABLE locations;"
+    )
+  end
+  
   test "database connection" do
     # perform a query first to 'wake-up' the connection
     assert_equal(query_scalar("SELECT 5").to_i, 5)
@@ -16,10 +31,14 @@ class TestGeoFoo < ActiveSupport::TestCase
   test "as_point" do
     latitude = 5
     longitude = 42
-    point = GeoFoo::Core.as_point latitude, longitude
+    point = GeoFoo.as_point latitude, longitude
     assert(query("SELECT #{point}"))
     assert_equal(query_scalar("SELECT ST_X(#{point})").to_i, 42)
     assert_equal(query_scalar("SELECT ST_Y(#{point})").to_i, 5)
+  end
+  
+  test "ar" do
+    assert_not_nil Location
   end
   
 end
